5c8c2f8fc712401c5beeea98e36ee533d1231fd7
Rafael Alves <168144782+rafaelAlvesQaTester@users.noreply.github.com>
2025-07-24 23:58:36 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+console.log(5+5)
+
+console.log("Olá Rafael!")
+
+// declarando variaveis
+//var, let const
+
+var a = 1;
+var b = 5;
+var c = a + b;
+console.log(c);
+
+// é case sensitive ou seja, sensivel a maiusculo e minusculo.
+
+var PESSOA = "MARCOS";
+var pessoa = "Rafael";
+console.log(pessoa);
+
